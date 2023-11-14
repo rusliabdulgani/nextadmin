3c67dd355ad90712429d8fa50ef1770c92496336
rusliabdulgani <rusli.gani88@gmail.com>
2023-11-14 20:13:59 +0700

    add chart & righbar in dashboard page

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,54 @@
+import Image from 'next/image';
 import styles from './rightbar.module.css';
+import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md';
 
 function Rightbar() {
-  return <div className={styles.container}>Rightbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image
+            src="/astronaut.png"
+            alt="astronaut"
+            fill
+            className={styles.bg}
+          />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit eius libero perspiciatis recusandae possimus.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit eius libero perspiciatis recusandae possimus.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
